7499fd1c92e5229cb44ba4f0f9542f483b672e35
matt <mattryanhere@pm.me>
2025-09-18 17:48:35 -0700

    login and out

---
+++ src/app/lib/actions.js
@@ -0,0 +1,27 @@
+"use server";
+
+import { signIn } from '../../../auth';
+import { AuthError } from 'next-auth';
+
+export async function authenticate(
+    prevState,
+    formData,
+) {
+    try {
+        await signIn('credentials', {
+            redirect: true,
+            redirectTo: '/ui/dashboard',
+            email: formData.get('password'),
+        });
+    } catch (error) {
+        if (error instanceof AuthError) {
+            switch (error.type) {
+                case 'CredentialsSignIn':
+                    return 'Invalid Credentials.';
+                default:
+                    return 'Something went wrong.';
+            }
+        }
+        throw error;
+    }
+}
